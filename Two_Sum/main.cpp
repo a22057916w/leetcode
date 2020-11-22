a7913be4e25793c2582b6194d06f33e3bfc87963
a22057916w <a22057916w@gmail.com>
2020-11-23 00:44:07 +0800

    test for arithmetic input

--- Two_Sum/main.cpp
+++ Two_Sum/main.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    static vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> cache;
         vector<int> answer;
 
         for(size_t i = 0; i < nums.size(); i++) {
           int needed_num = target - nums[i];
 
-          if(cache.find(needed_num) != cache.end) {
+          if(cache.find(needed_num) != cache.end()) {
             answer.push_back(cache[needed_num]);
             answer.push_back(i);
             return answer;
           }
           else
-            cache.insert(make_pair(num[i], i));
+            cache.insert(make_pair(nums[i], i));
         }
     }
 };
@@ -26,5 +26,10 @@ public:
 int main() {
   vector<int> nums = {2, 7, 11, 15};
   int target = 9;
-  cout << Solution.twoSum(nums, target) << endl;
+
+  vector<int> ans = Solution::twoSum(nums, target);
+  for(size_t n : ans)
+    cout << n << endl;
+
+  return 0;
 }
