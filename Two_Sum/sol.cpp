6ef77001ce445d174105329ee256d3b0c4cdbf96
a22057916w <a22057916w@gmail.com>
2020-12-03 21:08:14 +0800

    Create sol.cpp

---
+++ Two_Sum/sol.cpp
@@ -0,0 +1,28 @@
+// This is for leetcode submition
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    static vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> cache;
+        vector<int> answer;
+
+        for(size_t i = 0; i < nums.size(); i++) {
+          int needed_num = target - nums[i];
+
+          if(cache.find(needed_num) != cache.end()) {
+            answer.push_back(cache[needed_num]);
+            answer.push_back(i);
+            return answer;
+          }
+          else
+            cache.insert(make_pair(nums[i], i));
+        }
+        // in case things go wrong
+        return answer;
+    }
+};
