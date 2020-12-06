36c7248e83d301bcbc61ad5092576a70f4ef1355
a22057916w <a22057916w@gmail.com>
2020-12-07 00:31:36 +0800

    solved code

--- Remove_Duplicates_from_Sorted_Array/sol.cpp
+++ Remove_Duplicates_from_Sorted_Array/sol.cpp
@@ -5,20 +5,22 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-      int pos, target;
-      int cnt = 0;
+      if(nums.size() == 0)
+        return 0;
 
-      pos = 1;
-      target = nums[0];
-      for(size_t i = 1; i < nums.size(); ++i) {
+      int pos = 0, target = nums[0];
+      for(size_t i = 0; i < nums.size(); ++i) {
         if(target != nums[i]){
           target = nums[i];
-          nums.erase(nums.begin() + pos, nums.begin() + i);
-          cnt++;
+          nums.erase(nums.begin() + pos, nums.begin() + i - 1);
           i = ++pos;
         }
+        else if(i - pos > 0 && i == nums.size() - 1)
+          nums.erase(nums.begin() + pos, nums.begin() + i);
+        else
+          continue;
       }
 
-      return cnt;
+      return nums.size();
     }
 };
