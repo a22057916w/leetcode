86195ed9def4da1ab958d272986b502e7cd8321b
a22057916w <a22057916w@gmail.com>
2020-12-07 00:37:56 +0800

    adding comment

--- Remove_Duplicates_from_Sorted_Array/sol.cpp
+++ Remove_Duplicates_from_Sorted_Array/sol.cpp
@@ -8,14 +8,16 @@ public:
       if(nums.size() == 0)
         return 0;
 
+      // pos is the mark of the starting position when the vector rearrangents itself.
+      // target for tracking tht current value of nums[i]
       int pos = 0, target = nums[0];
       for(size_t i = 0; i < nums.size(); ++i) {
         if(target != nums[i]){
           target = nums[i];
           nums.erase(nums.begin() + pos, nums.begin() + i - 1);
-          i = ++pos;
+          i = ++pos;    // after the elimination taking place, the starting position must reset and move forward
         }
-        else if(i - pos > 0 && i == nums.size() - 1)
+        else if(i - pos > 0 && i == nums.size() - 1)    // for elimination like [1,1,1] or [0,1,1,1,1]
           nums.erase(nums.begin() + pos, nums.begin() + i);
         else
           continue;
