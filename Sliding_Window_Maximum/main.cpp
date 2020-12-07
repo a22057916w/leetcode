b108afb636c9449b86944a90a52992ea09deaefc
a22057916w <a22057916w@gmail.com>
2020-12-08 00:50:24 +0800

    phase1: dev complete

--- Sliding_Window_Maximum/main.cpp
+++ Sliding_Window_Maximum/main.cpp
@@ -1,40 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 using namespace std;
 
 class Solution {
 public:
     static vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+      deque<int> maxWindow;
+
+      // initiating the deque for the first sliding-window
+      for(int i = 0; i < k && i < nums.size(); i++) {
+        while(maxWindow.size() > 0 && nums[maxWindow.back()] <= nums[i])
+          maxWindow.pop_back();
+        maxWindow.push_back(i);
+      }
       vector<int> ans;
 
-      int maxn = 0, pos = 0;
-      for(int i = 0; i < nums.size(); i++) {
-          if(i == 0) {
-            for(int j = 0; j < k; j++)
-              maxn = max(maxn, nums[j]);
-            ans.push_back(maxn);
-            pos = k - 1;
-          }
-          else if(i < nums.size - k) {
-            maxn = maxn(maxn, nums[pos++])
-            ans.push_back(maxn);
-          }
-          else {
-            for(int j = i; j < nums.size(); j++)
-              maxn = max(maxn, nums[j]);
-            ans.push_back(maxn);
-          }
+      // dealing with the following sliding-windows with deqeu and vector
+      for(int i = k; i < nums.size(); i++) {
+        ans.push_back([maxWindow.front()]);
+
+        if(maxWindow.size() > 0 && i - maxWindow.front() >= k)
+          maxWindow.pop_front();
+        while(maxWindow.size() > 0 && nums[maxWindow.back()] <= nums[i])
+          maxWindow.pop_back();
+
+        maxWindow.push_back(i);
       }
-      for(auto n : nums)
-        cout << n << " ";
+
+      ans.push_back(nums[maxWindow.front()]);
       return ans;
     }
 };
-
 int main() {
-  vector<int> v = {1,3,-1,-3,5,3,6,7};
-  int k = 3;
-
+  vector<int> v = {9,10,9,-7,-4,-8,2,-6};
+  int k = 5;
+  vector<int> ans = Solution::maxSlidingWindow(v, k);
+  for(auto n : v)
+    cout << n << " ";
+  cout << endl;
+  for(auto n : ans)
+    cout << n << " ";
   return 0;
 }
