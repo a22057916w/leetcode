7cd0a93a81a5f2620c9a7c30c708ce405177f9b0
a22057916w <a22057916w@gmail.com>
2020-12-09 01:11:48 +0800

    complete algo

--- Pow/main.cpp
+++ Pow/main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Solution {
 public:
-    static double myPow(double x, int n) {
-        if(n == 0)
-            return 1;
-        double tmp = myPow(x, n / 2);
-        if(n % 2 == 0)
-            return tmp * tmp;
-        else {
-            if(n > 0)
-                return x * tmp * tmp;
-            else
-                return
-        }
+  class Solution {
+public:
+  static double myPow(double x, int n) {
+    double ans = powCur(x, abs(n));
+    if(n < 0) {
+      if(isinf(1 / ans))
+          return 0;
+      else
+          return 1 / ans;
     }
+    else
+      return ans;
+  }
+
+  static double powCur(double x, int n) {
+    if(n == 0)
+        return 1;
+    double tmp = myPow(x, n / 2);
+    if(n % 2 == 0)
+        return tmp * tmp;
+    else
+        return x * tmp * tmp;
+  }
 };
 
 int main() {
