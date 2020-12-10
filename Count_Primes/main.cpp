72ba739436c93c8e16aa052b5b65eafdd453623e
a22057916w <a22057916w@gmail.com>
2020-12-10 20:28:48 +0800

    dev prime-number list

---
+++ Count_Primes/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+private:
+    vector<int> prime;
+public:
+    static int countPrimes(int n) {
+        vector<int> prime(10, 0);
+    }
+
+    static void primelist() {
+        vector<bool> vis(false, size);
+        for(suto n: vis)
+            cout << n << endl;
+
+    }
+};
+
+int main() {
+  vector<int> v(10, 0);
+  //cout << Solution::countPrimes(10) << endl;
+  return 0;
+}
