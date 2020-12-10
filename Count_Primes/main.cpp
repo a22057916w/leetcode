983e9e5588b8067877e8d69c8df1a0fa62293845
a22057916w <a22057916w@gmail.com>
2020-12-10 21:37:01 +0800

    construct prime table

--- Count_Primes/main.cpp
+++ Count_Primes/main.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
 #include <vector>
-
+#include <cmath>
+#include <cstring>
 using namespace std;
 
 
 class Solution {
-private:
-    vector<int> prime;
 public:
     static int countPrimes(int n) {
-        vector<int> prime(10, 0);
+        vector<int> prime = primelist(n);   // get a list of primes
+
+        int total = 0;
+        for(int i = 0; i < prime.size(); i++)   // count the primes
+            if(prime[i] < n)
+                total++;
+            else
+                break;
+
+        return total;
     }
 
-    static void primelist() {
-        vector<bool> vis(false, size);
-        for(suto n: vis)
-            cout << n << endl;
+    static vector<int> primelist(int n) {
+        vector<bool> vis(n + 1, false);
+        vector<int> prime;
 
+        // producing prime number list
+        for(long long i = 2; i <= n; i++) {
+            if(vis[i])
+                continue;
+            else {
+                prime.push_back(i);
+                for(long long j = i * i; j <= n; j += i)
+                    vis[j] = true;
+            }
+        }
+        return prime;
     }
 };
 
 int main() {
   vector<int> v(10, 0);
-  //cout << Solution::countPrimes(10) << endl;
+  cout << Solution::countPrimes(10) << endl;
   return 0;
 }
