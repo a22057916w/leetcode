ba7d47002897f46e50110bfeb54368f6f85dd1aa
a22057916w <a22057916w@gmail.com>
2020-12-13 16:58:49 +0800

    using sort and unordered_map

---
+++ Number_of_Good_Pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        unordered_map<int, int> cnt;
+        for(int i = 0; i < nums.size(); i++) {
+            if(cnt.find(nums[i]) != cnt.end())
+                cnt[nums[i]]++;
+            else
+                cnt.insert(make_pair(nums[i], 0));
+        }
+
+        int ans = 0;
+        for(int i = 0; i < cnt.size(); i++)
+            ans += cnt[i] * (cnt[i] + 1) / 2;
+
+        return ans;
+    }
+};
