5900737e267d4deae1363751875d0b2a992d9f26
a22057916w <a22057916w@gmail.com>
2020-12-24 15:31:02 +0800

    write the instrustions and comment for the alogrithm

--- Palindrome_Linked_List.cpp
+++ Palindrome_Linked_List.cpp
@@ -1,14 +1,27 @@
+// The idea is find the middle of the list, then reverse
+// the rear part of the list.
+// For example, 1->2->2->1->NULL, the alogrithm goes
+//              f
+// 1->2->2->1->NULL
+//       s
+// where the iteration ends.
+// Then we move f to the head like 1->2->2->NULL and reverse s to 1->2->NULL,
+// and compare the two list until s goes to NULL
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
       ListNode *fast, *slow;
       fast = slow = head;
 
+      // Move the two pointers until they reach the proper positions
+      // in the list.
       while(fast != NULL && fast->next != NULL) {
         slow = slow->next;
         fast = fast->next->next;
       }
 
+      // compare the fast part(front end) and slow part(reversed rear end)
       fast = head;
       slow = reverse(slow);
 
