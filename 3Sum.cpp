d187afa4a6a261c3b4f884ebea50402dc0df82eb
a22057916w <a22057916w@gmail.com>
2020-12-27 23:51:18 +0800

    solved by two pointer method

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,33 +1,33 @@
 #include <algorithm>
-#include <unordered_map>
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        if(nums.size() < 3)
+          return {};
+
         sort(nums.begin(), nums.end());
 
-        vector<vector<int>> ans;
-        for(int i = 0; i < nums.size(); i++) {
-          unordered_map<int,int> cache;
+        vector<vector<int>> res;
+        for(int i = 0; i < nums.size() - 2; i++) {
+          if(nums[i] > 0) break;
+          if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-          for(int j = i + 1; j < nums.size(); j++) {
-            int needed_num = (0 - nums[i]) - nums[j];
+          int target = 0 - nums[i], j = i + 1, k = nums.size() - 1;
+          while(j < k) {
+            if(nums[j] + nums[k] == target) {
+              res.push_back({nums[i], nums[j], nums[k]});
 
-            if(cache.find(nums[j]) != cache.end()) {
-              vector<int> tmp;
-
-              tmp.push_back(nums[i]);
-              tmp.push_back(cache[nums[j]]);
-              tmp.push_back(nums[j]);
-
-              ans.push_back(tmp);
-              break;
+              while(j < k && nums[j] == nums[j + 1]) j++;
+              while(j < k && nums[k] == nums[k - 1]) k--;
+              j++; k--;
             }
+            else if(nums[j] + nums[k] < target)
+              j++;
             else
-              cache.insert(make_pair(needed_num, nums[j]));
+              k--;
           }
         }
-
-        return ans;
+        return res;
     }
 };
