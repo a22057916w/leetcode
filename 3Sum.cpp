7ce31ca06b20f49712fabb2884069ddef479478c
a22057916w <a22057916w@gmail.com>
2020-12-27 23:27:17 +0800

    unordered attempt failed, cannot deal with duplication

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,21 +1,33 @@
+#include <algorithm>
+#include <unordered_map>
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int size = nums.size();
-        vector<vector<int>> dp(size, vector<int>(size, 0));
+        sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < size; i++) {
-            dp[i][i] = nums[i];
-            for(int j = i + 1; j < size; j++) {
-                dp[i][j] = nums[i] + nums[j];
+        vector<vector<int>> ans;
+        for(int i = 0; i < nums.size(); i++) {
+          unordered_map<int,int> cache;
+
+          for(int j = i + 1; j < nums.size(); j++) {
+            int needed_num = (0 - nums[i]) - nums[j];
+
+            if(cache.find(nums[j]) != cache.end()) {
+              vector<int> tmp;
+
+              tmp.push_back(nums[i]);
+              tmp.push_back(cache[nums[j]]);
+              tmp.push_back(nums[j]);
+
+              ans.push_back(tmp);
+              break;
             }
+            else
+              cache.insert(make_pair(needed_num, nums[j]));
+          }
         }
 
-        for(int i = 1; i < size; i++)
-            for(int j = i + 1; j < size; j++) {
-                if(dp[i][j] + dp[i - 1][i] == 0) {
-                    vector<int> triple = {i, i}
-                }
-            }
+        return ans;
     }
 };
