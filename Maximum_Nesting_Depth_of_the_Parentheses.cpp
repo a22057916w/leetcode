49718461f977a51e8cec1b9b9ad8feacbeaa27b7
a22057916w <a22057916w@gmail.com>
2021-01-15 00:36:50 +0800

    counting method

---
+++ Maximum_Nesting_Depth_of_the_Parentheses.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int maxDepth(string s) {
+      int parentheses = 0, depth = 0;
+
+      for(int i = 0; i < s.length(); i++) {
+        if(s[i] == '(')
+          parentheses++;
+        if(s[i] == ')')
+          parentheses--;
+
+        // find the maxinum number of '(' - ')'
+        depth = max(depth, parentheses);
+      }
+
+      return depth;
+    }
+};
