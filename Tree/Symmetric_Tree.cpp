e7d3c73a686bd5e0c31f55a8ad472e08555d32d8
a22057916w <a22057916w@gmail.com>
2021-01-28 20:12:44 +0800

    tree.swap() like algo + DFS

--- Tree/Symmetric_Tree.cpp
+++ Tree/Symmetric_Tree.cpp
@@ -1,3 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 // The idea is similar to that compare two tree but in a symmtric
 // way(left to right; right to left).
 
