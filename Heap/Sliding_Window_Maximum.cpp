4452ce3f8c6ae262de98263c69ee855b0d242eed
a22057916w <a22057916w@gmail.com>
2021-01-31 00:16:13 +0800

    imp deque as a max heap

--- Heap/Sliding_Window_Maximum.cpp
+++ Heap/Sliding_Window_Maximum.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 using namespace std;
 
 // !!! brutal force, time limit exceeded !!!
 
 // using double-ended-queue as a container for sliding-window
-// the deque is a monotone decrease seqeuence. Therefore, the
+// the deque we implement is a monotone decrease seqeuence. Therefore, the
 // front of the deqeu is always the maximun value.
 // By using <deque>, we can solving the question in O(n).
 class Solution {
@@ -24,13 +25,17 @@ public:
 
       // dealing with the following sliding-windows with deqeu and vector
       for(int i = k; i < nums.size(); i++) {
+
+        // push the maxinum in each intervak
         ans.push_back(nums[maxWindow.front()]);
 
+        // moving one step forward then find new maximun in current interval
         if(maxWindow.size() > 0 && i - maxWindow.front() >= k)
           maxWindow.pop_front();
         while(maxWindow.size() > 0 && nums[maxWindow.back()] <= nums[i])
           maxWindow.pop_back();
 
+        // push the index of the forward number(nums[i])
         maxWindow.push_back(i);
       }
 
