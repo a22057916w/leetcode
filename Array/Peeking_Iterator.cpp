09174059aa5cd09a14a4ff7735b76377f05dd1ff
a22057916w <a22057916w@gmail.com>
2021-02-08 22:27:55 +0800

    second method: copy constructor

--- Array/Peeking_Iterator.cpp
+++ Array/Peeking_Iterator.cpp
@@ -18,6 +18,9 @@
 #define SOLUTION 1
 
 #if(SOLUTION == 0)
+
+// the concept is to store the next value to var value. And using flag to
+// determine wether peek() is occur before next()
 class PeekingIterator : public Iterator {
 private:
   bool flag;    // to determine it peek() is execute before next()
@@ -59,42 +62,23 @@ public:
 
 
 #else
+
+// using copy consturctor trick ClassName (const ClassName &old_obj);
+// e.g. Iterator(const Iterator& iter) -> Iterator(*this)
 class PeekingIterator : public Iterator {
-private:
-  bool flag;  // to determine it peek() is execute before next()
-  int value;    // store the from of next()
 public:
-	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-	    // Initialize any member here.
-	    // **DO NOT** save a copy of nums and manipulate it directly.
-	    // You should only use the Iterator interface methods.
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {}
 
-      flag = false;
-
-	}
-
-    // Returns the next element in the iteration without advancing the iterator.
 	int peek() {
-    if(!flag)
-      value = Iterator::next();
-
-    flag = true;
-    return value;
+    return Iterator(*this).next();
 	}
 
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
 	int next() {
-	  if(!flag)
-      return Iterator::next();
-    else {
-      flag = false;
-      return value;
-    }
+	  return Iterator::next();
 	}
 
 	bool hasNext() const {
-	  return flag ? value : Iterator::hasNext();
+	  return Iterator::hasNext();
 	}
 };
 
