304ab1ef8542db77ac9a462051d613ba43fa6d92
a22057916w <a22057916w@gmail.com>
2021-02-08 22:22:35 +0800

    deving second solution

--- Array/Peeking_Iterator.cpp
+++ Array/Peeking_Iterator.cpp
@@ -15,7 +15,50 @@
  *		bool hasNext() const;
  *	};
  */
+#define SOLUTION 1
 
+#if(SOLUTION == 0)
+class PeekingIterator : public Iterator {
+private:
+  bool flag;    // to determine it peek() is execute before next()
+  int value;    // store the from of next()
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+
+      flag = false;
+
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+    if(!flag)
+      value = Iterator::next();
+
+    flag = true;
+    return value;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	  if(!flag)
+      return Iterator::next();
+    else {
+      flag = false;
+      return value;
+    }
+	}
+
+	bool hasNext() const {
+	  return flag ? value : Iterator::hasNext();
+	}
+};
+
+
+#else
 class PeekingIterator : public Iterator {
 private:
   bool flag;  // to determine it peek() is execute before next()
@@ -54,3 +97,5 @@ public:
 	  return flag ? value : Iterator::hasNext();
 	}
 };
+
+#endif // SOLUTION
