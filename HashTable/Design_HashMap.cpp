cf3f2c837f043eb18cc6584cef5bc89209741b41
a22057916w <a22057916w@gmail.com>
2021-03-08 16:01:44 +0800

    hash function with Division method and STL list

--- HashTable/Design_HashMap.cpp
+++ HashTable/Design_HashMap.cpp
@@ -4,16 +4,15 @@
 class MyHashMap {
 private:
     vector<list<pair<int, int>>> table;
-    int elements;
 public:
     /** Initialize your data structure here. */
-    MyHashMap() : elements(0) {
+    MyHashMap() {
         table.resize(3000);     // m = 3000, load factor = 0.33333
     }
 
     /** value will always be non-negative. */
     void put(int key, int value) {
-        int index = key % table.size();   // perform hash huction
+        int index = key % table.size();   // perform hash fuction
         for(auto& it : table[index])
           if(it.first == key) {
             it.second = value;
@@ -21,27 +20,23 @@ public:
           }
 
         table[index].push_front(make_pair(key, value));
-        elements++;
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-      int index = key % table.size();   // perform hash huction
+      int index = key % table.size();   // perform hash fuction
       for(const auto& it : table[index])
-        if(it.first == key) {
-          elements--;
+        if(it.first == key) 
           return it.second;
-        }
 
         return -1;
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-      int index = key % table.size();   // perform hash huction
+      int index = key % table.size();   // perform hash fuction
       for(auto it = table[index].begin(); it != table[index].end(); it++)
         if(it->first == key) {
-          elements--;
           it = table[index].erase(it);
           return;
         }
