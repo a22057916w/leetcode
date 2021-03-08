1ca59366350111bc284209b0cd3c1c55a0a76cb9
a22057916w <a22057916w@gmail.com>
2021-03-08 15:25:31 +0800

    imp get() and put()

--- HashTable/Design_HashMap.cpp
+++ HashTable/Design_HashMap.cpp
@@ -1,21 +1,39 @@
 class MyHashMap {
 private:
     vector<list<pair<int, int>>> table;
-    int size, count;
+    int elements;
 public:
     /** Initialize your data structure here. */
-    MyHashMap() : size(20), count(0) {
-        table.resize(size);
+    MyHashMap() : elements(0) {
+        table.resize(20);
     }
 
     /** value will always be non-negative. */
     void put(int key, int value) {
-        if(key)
+        if((double)(elements / table.size()) > 0.5)
+          grow();
+
+        int index = key % table.size();   // perform hash huction
+        for(auto& it : table[index])
+          if(it.first == key) {
+            it.second = value;
+            return;
+          }
+
+        list.push_front(make_pair(key, value));
+        element++;
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
+      int index = key % table.size();   // perform hash huction
+      for(const auto& it : table[index[])
+        if(it.first == key) {
+          elements--;
+          return it.second;
+        }
 
+        return -1;
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
