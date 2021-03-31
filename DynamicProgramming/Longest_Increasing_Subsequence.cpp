3c3ad457520c38414e1b1d02d62bd737dd0519bd
a22057916w <a22057916w@gmail.com>
2021-03-31 22:57:54 +0800

    deving sol.1,  takes O(nn)

--- DynamicProgramming/Longest_Increasing_Subsequence.cpp
+++ DynamicProgramming/Longest_Increasing_Subsequence.cpp
@@ -1,7 +1,28 @@
 // https://leetcode.com/problems/longest-increasing-subsequence/
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+
+class Solution1 {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+      vector<int> lng(nums.size(), 1);
+
+      for(int i = 0; i < nums.size(); i++) {
+        for(int j = 0; j < i; j++)
+          if(nums[j] < nums[i])
+            lng[i] = max(lng[i], lng[j] + 1);
+      }
+
+      int maxl = INT_MIN;
+      for(const auto& e : lng)
+        maxl = max(maxl, e);
+
+      return maxl;
+    }
+};
 
 int main() {
   return 0;
