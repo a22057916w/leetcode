4c769513f7d538f3b4e3909f98c27cc7c60068f5
a22057916w <a22057916w@gmail.com>
2021-04-08 23:38:33 +0800

    new method bisec()

--- DynamicProgramming/Longest_Increasing_Subsequence.cpp
+++ DynamicProgramming/Longest_Increasing_Subsequence.cpp
@@ -31,3 +31,35 @@ public:
       return maxl;
     }
 };
+
+class Solution2 {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+      vector<int> lng(nums.size(), 1);
+
+      for(int i = 0; i < nums.size(); i++) {
+        // find out
+        for(int j = 0; j < i; j++)
+          if(nums[j] < nums[i])
+            lng[i] = max(lng[i], lng[j] + 1);
+      }
+
+      int maxl = INT_MIN;
+      for(const auto& e : lng)
+        maxl = max(maxl, e);
+
+      return maxl;
+    }
+
+    int bisec(vector<int>& tail, int left, int right, int key) {
+      while(left < right) {
+        int mid = (left + right) >> 1;
+        if(tail[mid] >= key)
+          right = mid;
+        else
+          left = mid;
+      }
+      
+      return right;
+    }
+};
