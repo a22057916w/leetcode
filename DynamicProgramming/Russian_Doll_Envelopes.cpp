b90a151ee30f447068cebb6a031e56fe9ca66620
a22057916w <a22057916w@gmail.com>
2021-05-02 01:35:15 +0800

    add ref

--- DynamicProgramming/Russian_Doll_Envelopes.cpp
+++ DynamicProgramming/Russian_Doll_Envelopes.cpp
@@ -1,9 +1,14 @@
+/*
+Reference:
+https://stackoverflow.com/questions/45494567/c-how-to-sort-the-rows-of-a-2d-vector-by-the-values-in-each-rows-column
+*/
+
 #include <alogrithm>
 #include <vector>
 
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-
+      sort(envelopes.begin(), envelopes.end())
     }
 };
