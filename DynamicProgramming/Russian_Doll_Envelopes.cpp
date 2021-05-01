ceefd582f97586c892003c2ccd795023f564f329
a22057916w <a22057916w@gmail.com>
2021-05-02 02:42:22 +0800

    add refs

--- DynamicProgramming/Russian_Doll_Envelopes.cpp
+++ DynamicProgramming/Russian_Doll_Envelopes.cpp
@@ -1,6 +1,9 @@
 /*
 Reference:
 https://stackoverflow.com/questions/45494567/c-how-to-sort-the-rows-of-a-2d-vector-by-the-values-in-each-rows-column
+https://stackoverflow.com/questions/7627098/what-is-a-lambda-expression-in-c11
+https://en.cppreference.com/w/cpp/algorithm/sort
+https://en.cppreference.com/w/cpp/named_req/Compare
 */
 
 #include <alogrithm>
