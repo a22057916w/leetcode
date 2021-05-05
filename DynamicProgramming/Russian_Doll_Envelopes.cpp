07554866ec12c98a042f3569f5c18cb4b2a0ddb0
a22057916w <a22057916w@gmail.com>
2021-05-05 17:15:47 +0800

    deving lIS with BISEC

--- DynamicProgramming/Russian_Doll_Envelopes.cpp
+++ DynamicProgramming/Russian_Doll_Envelopes.cpp
@@ -15,9 +15,35 @@ public:
       sort(envelopes.begin(), envelopes.end(), [](const auto& a, const auto& b){
         return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1])}; );
 
-      vector<int> tail;
-      for(const auto& envelope : envelopes) {
 
+      vector<int> tail(envelopes.size(), INT_MAX / 2);
+      int len = 1;
+
+      for(const auto& envl : envelopes) {
+        if(envl[1] < tail[0])
+          tail[0] = envl[1];
+        else if(envl[1] > tail[len - 1])
+          tail[len++] = envl[1];
+        else
+          tail[bisec()]
       }
+    }
+
+    int bisec(vector<int>& tail, int L, int R, int key) {
+      while(R - L > 1) {
+        int M = (R - L) / 2 + L;
+        if(key == tail[M])
+          return M;
+        else if(key > tail[M])
+          L = M + 1;
+        else
+          R = M - 1;
+      }
+
+      for(int i = L i <= R; i++)
+        if(tail[i] == key)
+          return i;
+
+      return -1;
     }
 };
