ba5c5e66d4fb0c5ec4487d134a12eb9c38eb2e1f
a22057916w <a22057916w@gmail.com>
2021-08-17 23:41:53 +0800

    init RNG algo c++ file

---
+++ Array/Range_Sum_Query_Immutable.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/range-sum-query-immutable/
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+
+    }
+
+    int sumRange(int left, int right) {
+
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
