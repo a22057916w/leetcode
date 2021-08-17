13556f084f0872158bf4c06289f6b3f9d4a718fc
a22057916w <a22057916w@gmail.com>
2021-08-18 00:25:33 +0800

    range sum query with prefix sum

---
+++ Array/Range_Sum_Query_Immutable.py
@@ -0,0 +1,21 @@
+class NumArray(object):
+    def __init__(self, nums):
+        arr = [0] * len(nums)
+
+        arr[0] = nums[0];
+        for i in range(1, len(nums)):
+          arr[i] = nums[i] + arr[i - 1]
+
+        self.pre = arr
+
+    def sumRange(self, left, right):
+        if(left == 0):
+          return(self.pre[right])
+        else:
+          return(self.pre[right] - self.pre[left - 1])
+
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
