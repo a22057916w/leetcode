5d817c2d787fe5228352bb906b9191da8dc164cd
a22057916w <a22057916w@gmail.com>
2021-08-17 23:59:36 +0800

    Range Sum Query with prefix sum

--- Array/Range_Sum_Query_Immutable.cpp
+++ Array/Range_Sum_Query_Immutable.cpp
@@ -1,14 +1,32 @@
+/*
+Reference:
+https://www.geeksforgeeks.org/range-sum-queries-without-updates/
+*/
+
 // https://leetcode.com/problems/range-sum-query-immutable/
+
+#include <vector>
+using std::vector;
 
 class NumArray {
 public:
     NumArray(vector<int>& nums) {
+      pre.resize(nums.size(), 0);
 
+      pre[0] = nums[0];
+      for(int i = 1; i < nums.size(); i++)
+        pre[i] = nums[i] + pre[i - 1];
     }
 
     int sumRange(int left, int right) {
-
+      if(left == 0)
+        return pre[right];
+      else
+        return pre[right] - pre[left - 1];
     }
+
+private:
+    vector<int> pre;
 };
 
 /**
